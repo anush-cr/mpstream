07038d1a5637938c7a394783633f77a9bc795cad
Anush Reddy <anush.cr@gmail.com>
2016-05-04 16:03:09 +0530

    Added audio player

--- Client/src/com/mpstream/Player.java
+++ Client/src/com/mpstream/Player.java
@@ -1,5 +1,6 @@
 package com.mpstream;
 
+import static com.mpstream.Player.timeToPlay;
 import com.xuggle.xuggler.Global;
 import com.xuggle.xuggler.IAudioSamples;
 import com.xuggle.xuggler.ICodec;
@@ -25,22 +26,22 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Player implements Runnable{
-    //The audio line we'll output sound to; it'll be the default audio device on your system if available
-    private static SourceDataLine mLine;
     //The window we'll draw the video on.
     private static VideoImage mScreen = null;
-    private static long mSystemVideoClockStartTime;
-    private static long mFirstVideoTimestampInStream;
+    //The audio line we'll output sound to; it'll be the default audio device on your system if available
+    public static SourceDataLine mLine;
     private Buffer buffer;
     private Thread t;
     private String threadName;
-    private static long videoStartTime;
     private static long startTime;
     private static long keyframeTimestamp;
+     private static long videoStartTime;
     private JFrame mainFrame;
     private ImageComponent videoScreen;
     private final Client client;
-    private int video = 0, audio = 0;
+    public int video = 0, rebuffer = 0;;
+    public boolean bufferFlag = true;
+    private Player player;
     
     private static void openJavaSound(IAudioSamples samples) throws LineUnavailableException {
         AudioFormat audioFormat = new AudioFormat(samples.getSampleRate(),
@@ -55,8 +56,18 @@ public class Player implements Runnable{
         //And if that succeed, start the line.
         mLine.start();
     }
+
+    private static void closeJavaSound() {
+        if (mLine != null) {
+            //Wait for the line to finish playing
+            mLine.drain();
+            //Close the line
+            mLine.close();
+            mLine=null;
+        }
+    }
     
-    private static long timeToPlay(IVideoPicture picture) {
+    public static long timeToPlay(long timestamp) {
         long millisecondsToSleep = 0;
         long currentTime = System.currentTimeMillis();
         long clockTimeSinceStartofVideo = currentTime - startTime;   
@@ -64,7 +75,7 @@ public class Player implements Runnable{
         // compute how long for this frame since the first frame in the stream.
         // remember that IVideoPicture and IAudioSamples timestamps are always in MICROSECONDS,
         // so we divide by 1000 to get milliseconds.
-        long streamTimeSinceStartOfVideo = (picture.getTimeStamp() - keyframeTimestamp)/1000;
+        long streamTimeSinceStartOfVideo = (timestamp - keyframeTimestamp)/1000;
         //System.out.println("streamTimeSinceStartOfVideo:"+streamTimeSinceStartOfVideo);
         final long tolerance = 50; // and we give ourselfs 50 ms of tolerance
         millisecondsToSleep = (streamTimeSinceStartOfVideo -
@@ -74,36 +85,18 @@ public class Player implements Runnable{
             millisecondsToSleep = 0;
         return millisecondsToSleep;
     }
-
-      
-    class audioStream {
-        private List audioList;
-        private int lastPacket = 0;
-        public audioStream(List l){
-            this.audioList = buffer.audioList;
-        }
-        
-    }
-    
-    class videoStream {
-        private List VideoList;
-        private int lastPacket = 0;
-        public videoStream(List l){
-            this.VideoList = buffer.videoList;
-        }
-    }
-    
+ 
     public Player(Buffer b, Client c) {
         threadName = "Player main thread";
         this.buffer = b;
         this.client = c;
         this.mainFrame = client.mainFrame;
         this.videoScreen = client.videoScreen;
+        this.player = this;
     }
     
     @Override
     public void run() {
-        int rebuffer = 0;
         try {
             client.statusLabel.setText("Status bar:\nBuffering");
             t.sleep(2500);     
@@ -111,14 +104,15 @@ public class Player implements Runnable{
             synchronized(buffer.audioList) {
                 while(buffer.audioList.indexOf(null) == 0) {}
                 ia = (IAudioSamples)buffer.audioList.get(0);
-                //openJavaSound(ia);
+                openJavaSound(ia);
             }
             synchronized(buffer.videoList) {
                 while(buffer.videoList.indexOf(null) == 0) {}
                 IVideoPicture vp = (IVideoPicture)buffer.videoList.get(0);
                 keyframeTimestamp = vp.getTimeStamp();
             }
-            boolean bufferFlag = true;
+            audioPlayer ap = new audioPlayer(buffer,client,player);
+            ap.start();
             for(;;) {
                 if(client.exitFlag)
                     break;
@@ -139,14 +133,14 @@ public class Player implements Runnable{
                 }
                 if(videoPic != null) {
                     if(bufferFlag) {
+                        bufferFlag = false;
                         startTime = System.currentTimeMillis();
                         videoStartTime = startTime;
                         keyframeTimestamp = videoPic.getTimeStamp();
                         videoScreen.setImage(Utils.videoPictureToImage(videoPic));
                         videoPic.delete();
-                        bufferFlag = false;
                     } else {
-                        t.sleep(timeToPlay(videoPic));
+                        t.sleep(timeToPlay(videoPic.getTimeStamp()));
                         videoScreen.setImage(Utils.videoPictureToImage(videoPic));
                         videoPic.delete();
                         mainFrame.validate();
@@ -160,6 +154,7 @@ public class Player implements Runnable{
         } catch (Exception ex) {
             Logger.getLogger(Player.class.getName()).log(Level.SEVERE, null, ex);
         }
+        closeJavaSound();
         client.exitFlag = true;
         client.statusLabel.setText("Status bar:\nFinished playing video");
         client.statusLabel.append("\nBuffer time: " + client.bufferTime);
@@ -168,11 +163,77 @@ public class Player implements Runnable{
         System.out.println("No of rebuffer events: " +  rebuffer);
         System.out.println("Thread " +  threadName + " exiting.");
     }
+    
     public void start() {
         System.out.println("Starting " +  threadName );
         if (t == null) {
             t = new Thread (this, threadName);
             t.start();
         }
+    }
+      
+    class audioPlayer implements Runnable {
+        private List audioList;
+        private int lastPacket = 0;
+        private Buffer buffer;
+        private Client client;
+        private Player player;
+        private Thread t;
+        private String threadName;
+        private int audio = 0;
+        
+        public audioPlayer(Buffer b, Client c, Player p){ 
+            this.threadName = "Audio Player thread";
+            this.buffer = b;
+            this.client = c;
+            this.player = p;
+        }
+
+        @Override
+        public void run() {
+            try {
+                for(;;) {
+                    if(client.exitFlag)
+                        break;
+                    if(player.bufferFlag) {
+                        t.sleep(3000);
+                    }
+                    IAudioSamples ia = null;
+                    synchronized(buffer.audioList) {
+                        if(buffer.audioList.indexOf(null) <= audio) {
+                            client.statusLabel.setText("Status bar:\nAudio Buffering");
+                            player.bufferFlag = true;
+                            player.rebuffer++;
+                            continue;
+                        } else {
+                            ia = (IAudioSamples)buffer.audioList.get(audio++);
+                        }
+                    }
+                    if(ia != null) {
+                        if(bufferFlag) {
+                            byte[] rawBytes = ia.getData().getByteArray(0, ia.getSize());
+                            player.mLine.write(rawBytes, 0, ia.getSize());
+                        } else {
+                            //t.sleep(timeToPlay(ia.getTimeStamp()));
+                            //play audio
+                            byte[] rawBytes = ia.getData().getByteArray(0, ia.getSize());
+                            player.mLine.write(rawBytes, 0, ia.getSize());
+                            ia.delete();
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            System.out.println("Thread " +  threadName + " exiting.");
+        }
+        public void start() {
+            System.out.println("Starting " +  threadName );
+            if (t == null) {
+                t = new Thread (this, threadName);
+                t.start();
+            }
+        }
+
     }
 }
